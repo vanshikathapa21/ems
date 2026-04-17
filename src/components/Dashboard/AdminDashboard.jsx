422e69cb33eb69d2b0c32f77168ae5af7a139213
vanshikathapa21 <vanshikathapa786@gmail.com>
2026-04-17 11:42:37 +0530

    Add admin task sections

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,28 +1,16 @@
 import React from 'react'
 import Header from '../other/Header'
+import CreateTask from '../other/CreateTask'
+import AllTask from '../other/AllTask'
 
 const AdminDashboard = () => {
   return (
-    <div className='h-screen w-full p-10'>
+    <div className='h-screen w-full p-7'>
       <Header />
+      <CreateTask />
+      <AllTask/>
 
-
-      <div>
-        <form>
-          <h3>Task Tittle</h3>
-          <input type="text" placeholder='Make a UI design'  />
-          <h3>Description</h3>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-          <h3>Date</h3>
-          <input type="date" />
-          <h3>Assign To</h3>
-          <input type="text" placeholder='employee name' />
-          <h3>Category</h3>
-          <input type="text" value="Create Task" placeholder='design,dev,etc.' />
-          <button>Create Task</button>
-        </form>
-
-      </div>
+      
     </div>
   )
 }
